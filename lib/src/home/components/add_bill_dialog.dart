ec65cc87b75c244dfebcb2f3777e6d31f99b25aa
brunowsus <brunoneyla@gmail.com>
2024-02-05 18:59:41 -0300

    edit_bill_upgrade_appearence

--- lib/src/home/components/add_bill_dialog.dart
+++ lib/src/home/components/add_bill_dialog.dart
@@ -7,7 +7,7 @@ class AddBillDialog extends StatelessWidget {
   final TextEditingController amountController;
   final VoidCallback onBillAdded;
 
-  const AddBillDialog({
+  AddBillDialog({
     required this.formKey,
     required this.nameController,
     required this.dueDateController,
@@ -19,66 +19,120 @@ class AddBillDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Adicionar nova conta'),
-      content: Form(
-        key: formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: nameController,
-              decoration: const InputDecoration(labelText: 'Nome'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor, insira o nome da conta.';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: dueDateController,
-              decoration: const InputDecoration(labelText: 'Data de vencimento'),
-              onTap: () async {
-                FocusScope.of(context).requestFocus(new FocusNode());
-                final DateTime? picked = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2100),
-                );
-                if (picked != null) {
-                  dueDateController.text =
-                      "${picked.day.toString().padLeft(2, '0')}/${picked.month.toString().padLeft(2, '0')}/${picked.year.toString()}";
-                }
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor, insira a data de vencimento.';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: amountController,
-              decoration: const InputDecoration(labelText: 'Valor (R\$)'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor, insira o valor.';
-                }
-                return null;
-              },
-            ),
-          ],
+      content: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    child: const Icon(Icons.text_fields),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Nome',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira o nome da conta.';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    child: IconButton(
+                      icon: const Icon(Icons.calendar_today),
+                      onPressed: () async {
+                        FocusScope.of(context).requestFocus(new FocusNode());
+                        final DateTime? picked = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        );
+                        if (picked != null) {
+                          dueDateController.text =
+                              "${picked.day.toString().padLeft(2, '0')}/${picked.month.toString().padLeft(2, '0')}/${picked.year.toString()}";
+                        }
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: dueDateController,
+                      decoration: const InputDecoration(
+                        labelText: 'Data de vencimento',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    child: const Text('R\$'),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: amountController,
+                      decoration: const InputDecoration(
+                        labelText: 'Valor',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira o valor.';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       actions: <Widget>[
-        TextButton(
-          child: const Text('Cancelar'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
+        Container(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red.shade100,
+            ),
+            child: const Text('Cancelar'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
         ),
-        TextButton(
-          child: const Text('Adicionar'),
-          onPressed: onBillAdded,
+        Container(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green.shade100,
+            ),
+            child: const Text('Adicionar'),
+            onPressed: onBillAdded,
+          ),
         ),
       ],
     );
--- lib/src/home/components/edit_bill_dialog.dart
+++ lib/src/home/components/edit_bill_dialog.dart
@@ -1,104 +1,181 @@
 import 'package:flutter/material.dart';
 import 'package:bill_manager_app/src/models/bill.dart';
 
-class EditBillDialog extends StatelessWidget {
-  final Bill bill;
+class EditBillDialog extends StatefulWidget {
   final GlobalKey<FormState> formKey;
   final TextEditingController nameController;
   final TextEditingController dueDateController;
   final TextEditingController amountController;
-  final VoidCallback onBillDeleted;
   final VoidCallback onBillSaved;
+  final VoidCallback onBillDeleted;
+  final Bill bill;
 
   EditBillDialog({
-    required this.bill,
     required this.formKey,
     required this.nameController,
     required this.dueDateController,
     required this.amountController,
-    required this.onBillDeleted,
     required this.onBillSaved,
+    required this.onBillDeleted,
+    required this.bill,
   });
 
   @override
+  _EditBillDialogState createState() => _EditBillDialogState();
+}
+
+class _EditBillDialogState extends State<EditBillDialog> {
+  bool isPaid = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isPaid = widget.bill.isPaid;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    widget.nameController.text = widget.bill.name;
+    widget.dueDateController.text =
+        "${widget.bill.dueDate.day.toString().padLeft(2, '0')}/${widget.bill.dueDate.month.toString().padLeft(2, '0')}/${widget.bill.dueDate.year.toString()}";
+    widget.amountController.text = widget.bill.amount.toString();
+
     return AlertDialog(
       title: const Text('Editar conta'),
       content: SingleChildScrollView(
         child: Form(
-          key: formKey,
+          key: widget.formKey,
           child: Column(
             children: <Widget>[
-              TextFormField(
-                controller: nameController,
-                decoration: const InputDecoration(labelText: 'Nome'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira o nome da conta.';
-                  }
-                  return null;
-                },
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    child: const Icon(Icons.text_fields),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: widget.nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Nome',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira o nome da conta.';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
               ),
-              TextFormField(
-                controller: dueDateController,
-                decoration: const InputDecoration(
-                    labelText: 'Data de vencimento (dd/mm/yyyy)'),
-                onTap: () async {
-                  FocusScope.of(context).requestFocus(new FocusNode());
-                  final DateTime? picked = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                  );
-                  if (picked != null) {
-                    dueDateController.text =
-                        "${picked.day.toString().padLeft(2, '0')}/${picked.month.toString().padLeft(2, '0')}/${picked.year.toString()}";
-                  }
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira a data de vencimento.';
-                  }
-                  return null;
-                },
+              SizedBox(height: 10),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    child: IconButton(
+                      icon: const Icon(Icons.calendar_today),
+                      onPressed: () async {
+                        FocusScope.of(context).requestFocus(new FocusNode());
+                        final DateTime? picked = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        );
+                        if (picked != null) {
+                          widget.dueDateController.text =
+                              "${picked.day.toString().padLeft(2, '0')}/${picked.month.toString().padLeft(2, '0')}/${picked.year.toString()}";
+                        }
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: widget.dueDateController,
+                      decoration: InputDecoration(
+                        labelText: 'Data de vencimento',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              TextFormField(
-                controller: amountController,
-                decoration:
-                    const InputDecoration(labelText: 'Valor (R\$)'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira o valor.';
-                  }
-                  return null;
-                },
+              SizedBox(height: 10),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    child: const Text('R\$'),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: widget.amountController,
+                      decoration: InputDecoration(
+                        labelText: 'Valor',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira o valor.';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
               ),
-              CheckboxListTile(
-                title: const Text('Pago'),
-                value: bill.isPaid,
-                onChanged: (bool? value) {
-                  bill.isPaid = value!;
-                },
+              SizedBox(height: 10),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    child: const Text('Pago'),
+                  ),
+                  Expanded(
+                    child: Checkbox(
+                      value: isPaid,
+                      onChanged: (bool? value) {
+                        if (value != null) {
+                          setState(() {
+                            isPaid = value;
+                            widget.bill.isPaid = value;
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
       ),
       actions: <Widget>[
-        TextButton(
-          child: const Text('Cancelar'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
+        Container(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red.shade100,
+            ),
+            child: const Text('Excluir'),
+            onPressed: widget.onBillDeleted,
+          ),
         ),
-        TextButton(
-          child: const Text('Deletar'),
-          onPressed: onBillDeleted,
-        ),
-        TextButton(
-          child: const Text('Salvar'),
-          onPressed: onBillSaved,
+        Container(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green.shade100,
+            ),
+            child: const Text('Salvar'),
+            onPressed: widget.onBillSaved,
+          ),
         ),
       ],
     );
